c815d09699c1e1a4a7c488bc42fcc2528d5875f3
comoque1 <comoque@gmail.com>
2020-07-22 15:26:58 +0300

    Updated void_payment_transaction tests  -  rosponse variables to correspond to the request type   -  fixed: should not be processed - use valid void unique_id   - added: should not be processed - using unique_id from declined*

--- test/void_payment_transactions.spec.js
+++ test/void_payment_transactions.spec.js
@@ -2,53 +2,84 @@
 
 import { expect } from "chai";
 import "regenerator-runtime/runtime.js";
-import { deleteTransactionRequest, saleRequest, voidRequest } from "../api/payment-transation-requests";
+import { saleRequest, voidRequest } from "../api/payment-transation-requests";
 import { saleTransactionDefaults } from './sale_payment_transactions.spec';
+import { async } from "regenerator-runtime/runtime.js";
 
 describe('Void payment transactions', function () {
-  before(async function () {
-    let response = await saleRequest(saleTransactionDefaults);
-    let parsedResponse = JSON.parse(response.body);
+  beforeEach(async function () {
+    let saleResponse = await saleRequest(saleTransactionDefaults);
+    let parsedResponse = JSON.parse(saleResponse.body);
     this.saleTransactionId = parsedResponse.unique_id;
-  })
-
-  after(async function () {
-    await deleteTransactionRequest(this.transactionId)
   });
 
   it('should be sent and approved', async function () {
     // Act
-    let response = await voidRequest(this.saleTransactionId)
-    let parsedResponse = JSON.parse(response.body);
+    let voidResponse = await voidRequest(this.saleTransactionId)
+    let voidParsedResponse = JSON.parse(voidResponse.body);
 
     //Assert
-    expect(response.statusCode).to.be.equal(200);
-    expect(parsedResponse.status).to.be.equal('approved');
-    expect(parsedResponse.message).to.be.equal('Your transaction has been voided successfully');
+    expect(voidResponse.statusCode).to.be.equal(200);
+    expect(voidParsedResponse.unique_id).not.to.be.null;
+    expect(voidParsedResponse.status).to.be.equal('approved');
+    expect(voidParsedResponse.usage).to.be.equal('Coffeemaker');
+    expect(voidParsedResponse.amount).to.be.equal(500);
+    expect(voidParsedResponse.transaction_time).not.to.be.null;
+    expect(voidParsedResponse.message).to.be.equal('Your transaction has been voided successfully');
   });
 
-  it('should be invalid - void transition using void valid reference_id', async function () {
+  it('should not be processed - use seconed time valid sale unique_id', async function () {
     // Act
-    let response = await voidRequest(this.saleTransactionId)
-    let parsedResponse = JSON.parse(response.body);
-    let voidTransactionId = parsedResponse.unique_id;
+    await voidRequest(this.saleTransactionId);
+    let voidResponse = await voidRequest(this.saleTransactionId);
+    let voidParsedResponse = JSON.parse(voidResponse.body);
+
+    //Assert
+    expect(voidResponse.statusCode).to.be.equal(422);
+    expect(voidParsedResponse.reference_id[0]).to.be.equal('Invalid reference transaction!');
+  });
+
+  it('should not be processed - use valid void unique_id', async function () {
+    // Act
+    let voidResponse = await voidRequest(this.saleTransactionId)
+    let voidParsedResponse = JSON.parse(voidResponse.body);
+    let voidTransactionId = voidParsedResponse.unique_id;
     let responseToExistingVoidTransaction = await voidRequest(voidTransactionId);
     let parsedResponseToExistingVoid = JSON.parse(responseToExistingVoidTransaction.body);
 
     //Assert
-    expect(response.statusCode).to.be.equal(422);
-    expect(parsedResponseToExistingVoid.reference_id[0]).to.be.equal('Invalid reference transaction!');
+    expect(responseToExistingVoidTransaction.statusCode).to.be.equal(422);
+    expect(parsedResponseToExistingVoid.reference_id[0]).to.be.contain('Invalid reference transaction!');
   });
 
-  it('should be invalid - void transition using invalid reference_id', async function () {
+  it('should not be processed - invalid void reference_id, async function', async function () {
     // Act
-    let response = await voidRequest('invalid reference_id')
-    let parsedResponse = JSON.parse(response.body);
+    let voidResponse = await voidRequest('invalid void reference_id');
+    let voidParsedResponse = JSON.parse(voidResponse.body);
 
     //Assert
-    expect(response.statusCode).to.be.equal(422);
-    expect(parsedResponse.reference_id[0]).to.be.equal('Invalid reference transaction!');
+    expect(voidResponse.statusCode).to.be.equal(422);
+    expect(voidParsedResponse.reference_id[0]).to.be.equal('Invalid reference transaction!');
   });
+});
 
+describe('Void payment transactions - declined sale transaction', function () {
 
+  it('should not be processed - using unique_id from declined sale transaction', async function () {
+    // Arrange
+    let saleResponse = await saleRequest({
+      ...saleTransactionDefaults,
+      card_number: '7700000000000000'
+    });
+    let parsedResponse = JSON.parse(saleResponse.body);
+    let saleTransactionId = parsedResponse.unique_id;
+
+    // Act
+    let voidResponse = await voidRequest(saleTransactionId)
+    let voidParsedResponse = JSON.parse(voidResponse.body);
+
+    //Assert
+    expect(voidResponse.statusCode).to.be.equal(422);
+    expect(voidParsedResponse.reference_id[0]).to.be.equal('Invalid reference transaction!');
+  });
 });
